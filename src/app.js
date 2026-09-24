8171f13918377da935a7a8a68b9a343e730665c0
Sanjay <sanjay2808a@gmail.com>
2026-09-24 18:41:55 +0530

    modified cors

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import errorHandler from "./middleware/errorHandler.js";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5174",
+    origin: "http://localhost:5173",
   })
 );
 
